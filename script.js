82b100f3c976d2675ce8fd58146ffb75af23afa6
8anooceu <jaocarrilhooo2903@gmail.com>
2024-10-28 12:59:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.onload = function() {
-  // Obtém o nome do dispositivo e personaliza a saudação
-  const userDeviceName = navigator.userAgentData ? navigator.userAgentData.brands[0].brand : "Visitante";
-  document.getElementById('saudacao').textContent = `Olá, ${userDeviceName}!`;
-};
+document.addEventListener("DOMContentLoaded", () => {
+    const saudacao = document.getElementById("saudacao");
+
+    // Obtém o nome do dispositivo se disponível e exibe
+    let deviceName = navigator.userAgentData?.brands[1]?.brand || navigator.userAgentData?.brands[0]?.brand || "Visitante";
+    saudacao.textContent = `Olá, ${deviceName}!`;
 
 function abrirPDF(pdf) {
   // Abre o PDF em uma nova aba para visualização em tela cheia
