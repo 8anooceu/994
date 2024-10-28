328daff1c6b5d8752f7a68fa51d8c1b0de33ee9e
8anooceu <jaocarrilhooo2903@gmail.com>
2024-10-28 12:35:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pdfLinks = document.querySelectorAll('.preview a');
-    const loadingScreen = document.getElementById('loadingScreen');
-    const loadingMessage = document.getElementById('loadingMessage');
-
-    pdfLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-
-            // Gera um número aleatório de 1 a 10 para a posição na fila
-            const queueNumber = Math.floor(Math.random() * 10) + 1;
-            loadingMessage.innerText = `Você é o número ${queueNumber} na fila...`;
-
-            // Mostra a tela de carregamento
-            loadingScreen.style.display = 'flex';
-
-            // Simula um tempo de espera antes de redirecionar para o PDF
-            setTimeout(() => {
-                window.open(link.getAttribute('data-link'), '_blank');
-                loadingScreen.style.display = 'none';
-            }, 3000);
-        });
-    });
-});
+window.onload = function() {
+  // Puxa o nome do dispositivo e personaliza a saudação
+  const userDeviceName = navigator.userAgentData ? navigator.userAgentData.brands[0].brand : "Visitante";
+  document.getElementById('saudacao').textContent = `Olá, ${userDeviceName}!`;
+};
