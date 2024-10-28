72185477f96f00ae216fd945b96c26ff254a606c
8anooceu <jaocarrilhooo2903@gmail.com>
2024-10-28 10:52:49 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Função para carregar imagens
-function loadImages() {
-    const imageInputs = [document.getElementById('image1'), document.getElementById('image2')];
-    const imagePreviews = document.getElementById('imagePreviews');
-
-    imageInputs.forEach(input => {
-        input.addEventListener('change', function () {
-            const file = this.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function (e) {
-                    const img = document.createElement('img');
-                    img.src = e.target.result;
-                    imagePreviews.appendChild(img);
-                };
-                reader.readAsDataURL(file);
-            }
-        });
-    });
-}
-
 // Função para carregar PDFs
 function loadPDFs() {
     const pdfInputs = [document.getElementById('pdf1'), document.getElementById('pdf2')];
@@ -38,8 +17,5 @@ function loadPDFs() {
     });
 }
 
-// Carregar imagens e PDFs quando o conteúdo estiver pronto
-document.addEventListener('DOMContentLoaded', () => {
-    loadImages();
-    loadPDFs();
-});
+// Carregar PDFs quando o conteúdo estiver pronto
+document.addEventListener('DOMContentLoaded', loadPDFs);
