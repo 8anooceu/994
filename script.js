5ce254ecaf8ea30ea9d15d33c48879305c2baf80
8anooceu <jaocarrilhooo2903@gmail.com>
2024-10-28 12:39:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 window.onload = function() {
-  // Puxa o nome do dispositivo e personaliza a saudação
+  // Obtém o nome do dispositivo e personaliza a saudação
   const userDeviceName = navigator.userAgentData ? navigator.userAgentData.brands[0].brand : "Visitante";
   document.getElementById('saudacao').textContent = `Olá, ${userDeviceName}!`;
 };
+
+function abrirPDF(pdf) {
+  // Abre o PDF em uma nova aba para visualização em tela cheia
+  window.open(pdf, '_blank');
+}
